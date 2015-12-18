603f1b72c79300d842b2c07da8f3e102ab3e11e5
yhung119 <yhung119@illinois.edu>
2015-12-18 08:47:56 -0500

    dropItem and pick item

--- src/adventure/core.clj
+++ src/adventure/core.clj
@@ -6,19 +6,21 @@
 ; [clojure.core.match :refer [match]]
 ; [clojure.string :as str]
 (def the-map
-  {:bedroom{:desc "There is a bed, and a warm warm warm blanket that is pretty effective in putting you to sleep"
+  {
+   :bedroom {:desc "There is a bed, and a warm warm warm blanket that is pretty effective in putting you to sleep"
            :todo "south to the hallway "
            :title "in the bedroom"
            :dir {:south :hallway
                   }
-           :content ["pen"]
-           } 
+           :content {:egg "egg"
+                     :what "what"}
+           }
    :living_room {:desc "There is a TV, laptop, and a good life. But you shouldn't have a good life. "
               :title "in the living room"
               :todo "east to the hallway"
               :dir {:east :hallway
                   }
-              :content [:calculator]
+              :content {:calculator "calculator"}
             }
    :hallway {:desc "It is very short, but not too short. "
               :title "the hallway"
@@ -27,33 +29,33 @@
                     :south :door
                     :east :kitchen
                     :west :living_room }
-              :content []}
+              :content {}}
    :kitchen {:desc "It looks like you could cook food here. "
               :title "the kitchen"
               :todo "west to the hallway"
               :dir {:west :hallway
                }
-              :content ["egg"]}
+              :content ["egg" "ggwi"]}
    :door {:desc "a door"
           :title "a door"
-          :todo "north to the hallway" 
+          :todo "north to the hallway"
           :dir {:north :hallway
                 :south :eceb
                 :east :siebal
 
             }
-          :content []}
+          :content {}}
     :eceb {:desc "a good place, but worse than Siebal"
            :title "ECEB"
            :todo "go back to the door"
            :dir {:north :door}
-           :content []
+           :content {}
          }
-    :siebal{:desc "go there and study! :)"
+    :siebal {:desc "go there and study! :)"
             :title "Siebal"
             :todo "west to the door"
             :dir {:west :door}
-            :content []
+            :content {}
     }
    })
 
@@ -69,10 +71,11 @@
 (defn status [adv]
   (let [location (adv :location)
         inventory (adv :inventory)]
-    (print (str "You are at " (-> the-map location :title) " and your inventory have. "))
+    (print (str "You are at " (-> the-map location :title)"."))
     (when-not ((adv :before) location)
       (print (-> the-map location :desc) ) )
     (update-in adv [:before] #(conj % location))))
+
 
 (defn go [dir adv]
   (let [curr-room (get-in adv [:location])]
@@ -82,22 +85,44 @@
          adv) )))
 
 (defn pick [obj adv]
-  (let [curr-room (get-in adv [:location])]
+  (let [curr-room (get-in adv [:location])
+       inv (get-in adv [:inventory])]
    (if-let [dest (get-in the-map [curr-room :content obj])]
-     (do (println "You cannot go that direction. " (-> the-map curr-room :content 0))(update-in adv [:inventory] dest))
-     (do (println "You cannot go that direction. " (-> the-map curr-room :content 0))
-        adv) ))) 
-
+    (if (contains? inv dest) (do (println "You already have the item") adv)
+        (update-in adv [:inventory] #(assoc % dest)))
+     (do (println "There is no such item. ")
+        adv) )))
+(defn dropItem [obj adv]
+    (if-let [dest (get-in adv [:inventory obj])]
+      (update-in adv [:inventory] #(dissoc % dest))
+      (do (println "You don't have such item")
+       adv)))
+(defn print_inventory [adv]
+    (do (println (seq (adv :inventory)))
+      adv))
+(defn print_content [adv]
+    (let [location (adv :location)]
+    (do (print (str (-> the-map location :content)))adv)))
 (defn respond [inst adv]
+  (if (contains? inst 1)
+  (match [(inst 0)]
+          [:pick] (pick (inst 1) adv)
+          [:dropItem] (drop (inst 1) adv)
+          [_](do
+              (println (str "I'm sorry Dave. I cannot allow you to do that."))
+              adv)
+          )
   (match inst
          [:north] (go :north adv)
          [:south] (go :south adv)
          [:west] (go :west adv)
          [:east] (go :east adv)
-         [:pick] (pick inst adv)
-         _ (do 
-             (println "I'm sorry Dave.  I cannot allow you to do that.")
-             adv) ) )
+         [:inventory] (print_inventory adv)
+         [:content] (print_content adv)
+         _ (do
+             (println (str "I'm sorry Dave. I cannot allow you to do that."))
+             adv) )
+    ) )
 
 (defn to-keywords [st]
    (mapv keyword (str/split st #" +")))
@@ -105,15 +130,17 @@
 (defn -main
   [& args]
   (println "What is your name")
-  (let [adv-name (adventurer :name)]
-        )
-  (println "Good morning! Fellow UIUC student, how was your sleep? Did you say you slept well eh? oh well I will make sure today is your nightmare, get ready kiddos")
+  (let [n (read-line)
+        adv-n (adventurer :name)
+        adv (assoc-in adventurer [:name] n)]
+  (println (str "Good morning, "(str(adv :name))"! How was your sleep? "))
   (loop [the-m the-map
          the-a adventurer]
     (let [location (the-a :location)
           the-a' (status the-a)
-          _      (println "What do you wanna do? You can go"  (str(-> the-m location :todo))) 
+          _      (println (str " What do you wanna do? You can go "  (str(-> the-m location :todo))))
           inst   (read-line) ]
       (recur the-m (respond (to-keywords inst) the-a'))
       ) ) )
+  )
 ; (println "What do you want to do? You can go" (str (-> the-m (the-a :location) :title)))
