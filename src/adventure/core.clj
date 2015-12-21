ae9f518d42cd142fdf833ee8e0613c66e39216e2
yhung119 <yhung119@illinois.edu>
2015-12-21 15:18:33 -0500

    fix
    fight, fixed the bug when adv defeat exam or exam defeat adv the game
    got stuck
    added talk and updated the description

--- src/adventure/core.clj
+++ src/adventure/core.clj
@@ -36,7 +36,7 @@
    :living_room {:desc "There is a TV, laptop, a calculator, and your key. But your key is locked by an evil puzzle that your roommate made last night. He thought you couldn't solve it, but you should reach for your potential and really, he didn't know you that well. The Master of puzzle."
               :title "the living room "
               :todo "east to the hallway"
-              :dir {:east :hallway
+              :dir {:east :hallway2
                   }
               :puzzle [
                     {:question "Fill in the blank. The following is the lyrcis from a song by Taylor Swift: I don't know about you. But I feel like __."
@@ -54,7 +54,6 @@
                     {:question "What is the remainder when 2 ^ 212 divdes 11 ?"
                      :answer "4"
                      :hint ""}
-
               ]
               :content #{"calculator"}
             }
@@ -62,7 +61,7 @@
    :hallway2 {:desc "It is very short, but not too short. "
               :title "the hallway next to the door"
               :todo "north: bedroom, south: door, east: kitchen, west: living room"
-              :dir {:north :bedroom
+              :dir {:north :hallway1
                     :south :door
                     :east :kitchen
                     :west :living_room }
@@ -71,7 +70,7 @@
    :kitchen {:desc "There is a meal on the table. You should do something with it."
               :title "the kitchen "
               :todo "west to the hallway"
-              :dir {:west :hallway
+              :dir {:west :hallway2
                }
               :content #{"meal"}}
 
@@ -106,7 +105,6 @@
             :title "Siebal "
             :dir {:north :road
                   :down :siebal0
-                  :in :acm
                   :south :eceb
                   :east :s_quad
                 }
@@ -117,10 +115,6 @@
               :title "Siebal Basement"
               :dir {:up :siebal}
               :content #{}}
-
-    :acm {:desc "people here are cool, but everyone is busy with exams, so "
-          :title "ACM Office"
-          :dir {:out :siebal}}
 
     :s_quad {:desc "how much you wish if you could just lay on the grass and take a nap"
              :title "the South Quad"
@@ -135,13 +129,25 @@
                   :west :s_quad
                   :north :green
                   :east :bus
+                  :find :seat
+                  :in :seat
                }
+               :content #{}
              }
+    :seat {:desc "There is a girl sitting in front of you, a really cute girl. You should try to talk to her, not like you will lose anything. If not you may leave after you study"
+           :title "A seat in Grainger"
+           :dir {
+                :leave :grainger
+                :out :grainger
+             }
+            :content #{}
+           }
     :green {:desc "it's green street, anything else other than bars here?"
             :title "Green Street"
             :dir {:south :grainger
                   :west :bar
-                  }}
+                  }
+                :content #{}}
 
     :bar {:desc "What do you think you can do here? Get some alochol? Well good guess"
           :title "Bar"
@@ -149,22 +155,24 @@
           :dir { :east :green}
           :content #{"alcohol"}
           }
-    ; :crce {:decs "Seems like you can exercise here"
-    ;        :title "CRCE"
-    ;        :dir {:south :green}}
+
     :bus {:desc "Welcome to the UIUC bus station that can take you to Chicago in just a second but does not exist in reality..."
           :title "the Bus Station"
           :dir {
              :west :grainger
              :take :chicago
-            }}
+            }
+          :content #{}
+          }
     :chicago {:desc "Welcome to Chicago, there is a bus that can take you back to UIUC in just a sec but doesn't exist in reality...\n However, this place seems much nicer than campus"
               :title "Chicago"
               :dir {
                 :take :bus
                 :south :chinatown
                 :north :willestower
-                }}
+                }
+            :content #{}
+              }
     :chinatown {:desc "You can buy a lot of foods here, and there is a Ktv store inside if you want to sing"
                 :title "ChinaTown"
                 :dir {
@@ -188,25 +196,42 @@
        })
 (def exam
   {:hp 200
-   :att 20})
-(def help "Useful Command:\n \n Directions:\n\n south/north/west/east\n\n Actions:\n\n pick/drop/puzzle/sleep/eat/cook/drink and some hidden actions\n\n Status:\n\n status(display your status)/inventory\n\n Fighting:\n\n attack(damange is equal to your skill points)\n use(use the items in your inventory...some might not be useful)\n You can also use command like inventory or status\n\n Abbreviation:\n\n 'i' for inventory\n 's' for status \n 'att' for attack\n")
+   :att 20
+   :defeat 0})
+(def help "Useful Command:\n \n Directions:\n\n south/north/west/east/up/down/in/out/find/leave/take\n\n Actions:\n\n pick(get)/drop/puzzle/sleep/eat/drink/jump/talk/fight and some hidden actions\n\n Status:\n\n status(display your status)/inventory(display your inventory items)\n\n Fighting:\n\n You need to 'fight' the monster before can do anything below\n attack(damange is equal to your skill points)\n use(use the items in your inventory...some might not be useful)\n You can also use command like inventory or status\n\n Abbreviation:\n\n 'i' for inventory\n 's' for status \n 'att' for attack\n\n Note 1:\n\n The hidden action teleport/tp takes a place for destination but some might not work because we aren't allow to have space when we name our locations\n\n Note 2:\n\n If you lost to the exam, you will lose all of your items\n")
 (def adventurer
   {:location :bedroom
-   :area :campus
    :name ""
    :hp 100
    :skill 10
-   :social 0
-   :inventory #{"key" "coffee" "alcohol"}
+   :social 10
+   :inventory #{}
    :before #{}
    :fight 0})
 
-(defn status [adv]
+;function for talk
+(defn talk [adv]
+  (let [curr-room (adv :location)]
+  (if (= curr-room :seat)
+      (if (>= (adv :social) 50)(do (println "The girl seemed to be interested in you... after a few weeks of talking you guys got together... You got a girlfriend")(update-in adv [:inventory] #(conj % "girlfriend")))
+                               (do (println "The girl doesn't seem interested in you...")adv)
+                               )
+    (do (println "No one to talk to here...")adv))))
+
+;the function for sleep which require the adv to be in the bedroom and restore the health to full
+(defn sleep [adv]
+ (let [curr-room (adv :location)]
+   (if (= curr-room :bedroom)
+     (do (println "You are at full health")(assoc-in adv [:hp] 100))
+     (do (println "You can't sleep here. Go back to the bedroom")adv))))
+
+(defn status [adv mon]
   (if (< (adv :hp) 1) (do (println "You have moved back to the bedroom")(sleep (assoc-in (assoc-in adv [:fight] 0) [:location] :bedroom)))
-  (let [fight (adv :fight)]
-      (if (= fight 1) adv
+  (let [fight (adv :fight)
+        de (mon :defeat)]
+      (if (and (= fight 1)(= de 0)) adv
   (let [location (adv :location)]
-    (print (str "You are at " (-> the-map location :title)"."))
+    (print (str "You are at " (-> the-map location :title)))
     (when-not ((adv :before) location)
       (print (-> the-map location :desc) ) )
     (update-in adv [:before] #(conj % location)))))))
@@ -216,10 +241,12 @@
   (do (println (str "Name: "(-> adv :name)". HP: "(-> adv :hp) ". Skill Point: " (-> adv :skill) ". Social Point: " (-> adv :social)))adv))
 
 ;fight : get into fighting mode
-(defn fighting [adv]
-  (let [curr-room (get-in adv [:location])]
+(defn fighting [adv mon]
+  (let [curr-room (get-in adv [:location])
+        de (mon :defeat)]
       (if (not (= curr-room :siebal0)) (do (println "There is no exam to fight here")adv)
-                                      (do (println "Get ready! You are fighting the exam")(assoc-in adv [:fight] 1)))))
+          (if (= de 0)(do (println "Get ready! You are fighting the exam")(assoc-in adv [:fight] 1))
+                      (do (println "You have already defeated the exam")adv)))))
 
 ;function for jump
 (defn jump [adv]
@@ -324,26 +351,22 @@
         (do (println "You can't go that way")adv))
       (do (println "You don't have the thing that allow you to do that yet")adv))))
 
-;the function for sleep which require the adv to be in the bedroom and restore the health to full
-(defn sleep [adv]
-  (let [curr-room (adv :location)]
-    (if (= curr-room :bedroom)
-      (do (println "You are at full health")(assoc-in adv [:hp] 100))
-      (do (println "You can't sleep here. Go back to the bedroom")adv))))
-
 ;monster attacked!
-(defn exam_respond [adv mon]
+(defn exam_respond [adv mon num obj]
     (let [curr-room (get-in adv [:location])
           att (rand-int (+ (mon :att) 1))
-          hp (get-in adv [:hp])]
-          (if (< (- hp att) 1) (do (println "You have lost all of your health")(update-in adv [:hp] - att))
+          hp (get-in adv [:hp])
+          mon_hp (get-in mon [:hp])]
+          (if (and (< mon_hp 50)(= obj "pokeball")) (assoc-in (update-in (update-in adv [:inventory] #(conj % "exam")) [:inventory] #(disj % obj)) [:fight] 0)
+          (if (< (- mon_hp num) 1) (do (println "You have defeated the exam")(assoc-in (update-in adv [:inventory] #(disj % obj)) [:fight] 0))
+          (if (< (- hp att) 1) (do (println "You have lost all of your health")(update-in (update-in adv [:inventory] #(disj % obj)) [:hp] - att))
           (cond
-            (= att 0) (do (println "The exam missed, your health is now"(str (- hp att)))(update-in adv [:hp] - att))
-            (<= att 5) (do (println "The exam used the Big Three, your health is now"(str (- hp att)))(update-in adv [:hp] - att))
-            (<= att 10) (do (println "The exam used KdTree, your health is now"(str (- hp att)))(update-in adv [:hp] - att))
-            (<= att 15) (do (println "The exam used Prim's Algorithm, your health is now"(str (- hp att)))(update-in adv [:hp] - att))
-            :else (do (println "The exam used some CS241 materials, your health is now"(str (- hp att)))(update-in adv [:hp] - att))
-          ))))
+            (= att 0) (do (println "The exam missed, your health is now"(str (- hp att)))(update-in (update-in adv [:inventory] #(disj % obj)) [:hp] - att))
+            (<= att 5) (do (println "The exam used the Big Three, your health is now"(str (- hp att)))(update-in (update-in adv [:inventory] #(disj % obj)) [:hp] - att))
+            (<= att 10) (do (println "The exam used KdTree, your health is now"(str (- hp att)))(update-in (update-in adv [:inventory] #(disj % obj)) [:hp] - att))
+            (<= att 15) (do (println "The exam used Prim's Algorithm, your health is now"(str (- hp att)))(update-in (update-in adv [:inventory] #(disj % obj)) [:hp] - att))
+            :else (do (println "The exam used some CS241 materials, your health is now"(str (- hp att)))(update-in (update-in adv [:inventory] #(disj % obj)) [:hp] - att))
+          ))))))
 
 ;the function for "status" which prints out the adv's current status
 (defn adv_status_mon [adv mon]
@@ -362,22 +385,28 @@
 
 ;use objects to att
 (defn using [adv mon obj]
-  (let [inv (adv :inventory)]
+  (let [inv (adv :inventory)
+        mon_hp (mon :hp)]
    (if (inv (name obj))
     (case (name obj)
       "calculator" (do (println "The exam lost 20 hp")(update-in mon [:hp] - 20))
-      "pokeball" (do (println "You tried to catch the exam... but you failed")mon)
-      "stones" (do (println "The exam lost 50 hp")(update-in mon [:hp] - 50))
-      "alcohol" (do (println "The exam gained 20 attack damage")(update-in mon [:att] + 20))
+      "pokeball" (if (>= mon_hp 50)(do (println "You tried to catch the exam... but you failed")mon) (do (println "1...2...3 You have caught the exam")(assoc-in mon [:defeat] 1)))
+      "stone" (do (println "The exam lost 50 hp")(update-in mon [:hp] - 50))
+      "coffee" (do (println "You have gain 10 attack damage")mon)
       "mic" (do (println "Your voice was powerful enought that exam lost 30 hp")(update-in mon [:hp] - 30))
       "cheatsheet" (do (println "It was super effective, the exam lost 70 hp")(update-in mon [:hp] - 70))
+      "telescop" (do (println "You looked through the exam, the exam lost 15 hp")(update-in mon [:hp] - 15))
+      "blanket" (do (println "You have recovered 30 health")mon)
+      "pillow" (do (println "You have recovered 10 health")mon)
       (do (println (str (name obj))"was not very effective")mon))
     (do (println "Hmm you don't have" (str (name obj))) mon)
       ) ) )
 
 (defn respond_monster [inst adv mon]
-  (let [state (adv :fight)]
+  (let [state (adv :fight)
+        mon_hp (mon :hp)]
   (if (= state 0)mon
+  (if (< mon_hp 1) mon
   (if (contains? inst 1)
     (match [(inst 0)]
       [:use] (using adv mon (inst 1))
@@ -395,20 +424,36 @@
       _ (do
           (println (str "I'm sorry "(-> adv :name)". I cannot allow you to do that."))
           mon) )
-    ))))
+    )))))
 
 (defn respond [inst adv mon]
   (let [state (adv :fight)
-        inv (adv :inventory)]
+        inv (adv :inventory)
+      ]
   (if (= state 1 )
+    (let [mon_hp (mon :hp)
+          skill (adv :skill)]
     (cond
-    (= (inst 0) :att) (exam_respond adv mon)
-    (= (inst 0) :attack) (exam_respond adv mon)
-    (= (inst 0) :use) (if (inv (name (inst 1))) (exam_respond adv mon) adv)
-    :else  adv)
+    (= (inst 0) :att) (exam_respond adv mon skill "")
+    (= (inst 0) :attack) (exam_respond adv mon skill "")
+    (= (inst 0) :use)   (if (inv (name (inst 1)))
+                            (case (name (inst 1))
+                             "calculator" (exam_respond adv mon 20 "calculator")
+                             "stone" (exam_respond adv mon 50 "stone")
+                             "mic" (exam_respond adv mon 30 "mic")
+                             "cheatsheet" (exam_respond adv mon 70 "cheatsheet")
+                             "pokeball" (exam_respond adv mon 0 "pokeball")
+                             "coffee" (exam_respond (update-in adv [:skill] + 10) mon 0 "coffee")
+                             "telescope" (exam_respond adv mon 0 "telescope")
+                             "blanket" (exam_respond (update-in adv [:hp] + 30)mon 0 "blanket")
+                             "pillow" (exam_respond (update-in adv [:hp] + 10)mon 0 "pillow")
+                             adv)
+                             adv)
+    :else  adv) )
   (if (contains? inst 1)
   (match [(inst 0)]
           [:pick] (pick (inst 1) adv)
+          [:get] (pick (inst 1) adv)
           [:drop] (discard (inst 1) adv)
           [:tp] (tp (inst 1) adv)
           [:drink] (drink (inst 1) adv)
@@ -426,6 +471,8 @@
          [:down] (go :down adv)
          [:in] (go :in adv)
          [:out] (go :out adv)
+         [:find] (go :find adv)
+         [:leave] (go :leave adv)
          [:take] (go :take adv)
          [:i] (print_inventory adv)
          [:inventory] (print_inventory adv)
@@ -435,9 +482,11 @@
          [:sleep] (sleep adv)
          [:puzzle] (puzzles adv)
          [:help] (do (println help)adv)
-         [:fight] (fighting adv)
+         [:fight] (fighting adv mon)
          [:jump] (jump adv)
          [:eat] (eat adv)
+         [:talk] (talk adv)
+
 
          _ (do
              (println (str "I'm sorry "(-> adv :name)". I cannot allow you to do that."))
@@ -446,13 +495,15 @@
 
 ;monster's stat
 (defn mon_stat [adv mon]
-  (let [fight (adv :fight)]
+  (let [fight (adv :fight)
+       monster_hp (get-in mon [:hp])]
+    (if (< monster_hp 1) (assoc-in mon [:defeat] 1)
     (if (= fight 0) mon
   (let [curr-room (get-in adv [:location])
-        monster_hp (get-in mon [:hp])]
-        (println "The monster's health is now"(str monster_hp) "\nWhat are you going to do next?")
-        mon
-        ))))
+        ]
+        (do (println "The monster's health is now"(str monster_hp) "\nWhat are you going to do next?")
+        mon)
+        )))))
 
 (defn to-keywords [st]
    (mapv keyword (str/split st #" +")))
@@ -469,7 +520,7 @@
          the-a adv'
          ]
     (let [location (the-a :location)
-          the-a' (status the-a)
+          the-a' (status the-a the-mon)
           the-mon' (mon_stat the-a' the-mon)
           _      (if (= (the-a :fight) 0) (println " What do you wanna do?") (print ""))
           inst   (read-line) ]
